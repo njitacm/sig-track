56d4665f77ae7f23bd5dc9d18ab716aeeccfc9cd
AOrps <aeo34@njit.edu>
2022-03-16 19:03:31 -0400

    revamped dynamic qr code generation + diff port

--- src/api/main.go
+++ src/api/main.go
@@ -1,27 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+
+	L "github.com/njitacm/sig-track/src/api/util"
 )
 
 const (
-	PORT = 9998
+	PORT      = 10233
+	BENDPOINT = "ec2-3-21-33-128.us-east-2.compute.amazonaws.com"
+	FENDPOINT = "http://localhost:10234"
 )
 
-type Ret struct {
-	Ucid string `json:"ucid"`
-	Sig  string `json:"sig"`
+type POSTREQ struct {
+	Sig, Ucid, Time string
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
+func handleRoot(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
+	case "GET":
+
 	case "POST":
 		fmt.Fprintf(w, "bruh")
 	default:
-		fmt.Fprintf(w, "POST plz!")
+		fmt.Fprintf(w, "Error!")
+	}
+}
+
+func handleGen(w http.ResponseWriter, r *http.Request) {
+	/*
+		ex:
+		handleGen:
+		http://localhost:9998/gen?sig=swe
+	*/
+
+	// enable cors
+	L.EnableCors(&w)
+
+	switch r.Method {
+	case "GET":
+		q := r.URL.Query()
+		sig := q.Get("sig")
+		if len(sig) == 0 {
+			fmt.Fprintf(w, "error in query, must have `sig={sig-name}`")
+		}
+		image := L.QRCodeGen(fmt.Sprintf("%s/%s", FENDPOINT, sig))
+		w.Write(image)
+	case "POST":
+		var res map[string]string
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&res)
+		L.Check(err)
+		sig := res["sig"]
+		image := L.QRCodeGen(fmt.Sprintf("%s/%s", FENDPOINT, sig))
+		w.Write(image)
+	default:
+		fmt.Fprintf(w, "No support yet!")
 	}
 }
 
@@ -29,7 +67,10 @@ func main() {
 	// L.QRCodeGen("ucid", "qr.png")
 	port := strconv.Itoa(PORT)
 
-	http.HandleFunc("/", root)
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/gen", handleGen)
+	// http.HandleFunc("/list", handleList)
+	// http.HandleFunc("/add", handleAdd)
 
 	fmt.Printf("http://localhost:%s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
--- src/api/util/qr.go
+++ src/api/util/qr.go
@@ -4,8 +4,14 @@ import (
 	qrcode "github.com/skip2/go-qrcode"
 )
 
-func QRCodeGen(content, filename string) {
+func QRCodeGenToFile(content, filename string) {
 	png, err := qrcode.Encode(content, qrcode.Medium, 256)
 	Check(err)
 	FileWrite(filename, png)
+}
+
+func QRCodeGen(content string) []byte {
+	png, err := qrcode.Encode(content, qrcode.Medium, 256)
+	Check(err)
+	return png
 }
